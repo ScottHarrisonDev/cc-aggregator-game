6c8e8806dbda0e5f90018679d3d9d109cedbf510
Scott Harrison <github@scottharrisondesign.co.uk>
2025-01-17 00:59:50 +0000

    update styling to be 2 columns of 3 on mobile

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,8 @@
-export const RESULT_LIMIT_COUNT = 5
+export const RESULT_LIMIT_COUNT = 6
 export const DATA_URL = "https://aw6hhyue7l-dsn.algolia.net/1/indexes/*/queries?x-algolia-api-key=de4ecaa4624ac11a92af5f7d7feb817b&x-algolia-application-id=AW6HHYUE7L"
+export const FILTER_WORDS = [
+    'Land Rover',
+    'Range Rover',
+    '911',
+    'RS6'
+]
